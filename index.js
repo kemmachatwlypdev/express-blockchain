9b94c7049d53d91430d5078d82fdc94fd003bd2b
Kemmachat Wanlayaphorn <70964204+kemmachat-wlyp@users.noreply.github.com>
2021-11-23 16:46:31 +0700

    class chain

--- index.js
+++ index.js
@@ -27,3 +27,54 @@ class Block {
         ).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.id = '';
+        this.name = '';
+        this.blockchain = '';
+        this.difficulty = '';
+    }
+
+    create(id, name, genesis) {
+        this.id = id;
+        this.name = name;
+        this.blockchain = [this.startGenesisblock(genesis)];
+        this.difficulty = 4;
+    }
+
+    startGenesisblock(genesis) {
+        return new block(
+            0,
+            genesis.date,
+            genesis.transaction,
+            "0"
+        )
+    }
+
+    obtainLastestBlock() {
+        return this.blockchain[this.blockchain.length - 1];
+    }
+
+    addNewBlock(newBlock) {
+        newBlock.precedingHash = this.obtainLastestBlock().hash;
+        newBlock.hash = newBlock.computeHash();
+        this.blockchain.push(newBlock);
+    }
+
+    checkChainValidity() {
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i];
+            const precedingBlock = this.blockchain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.computeHash()) {
+                return false;
+            }
+
+            if (currentBlock.precedingHash !== precedingBlock.hash()) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
